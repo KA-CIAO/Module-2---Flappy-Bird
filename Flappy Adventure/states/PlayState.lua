d24d71ff36dec504b6c080d32721baa9b0d866c2
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-11-23 17:20:52 +0800

    Update PlayState.lua

--- Flappy Adventure/states/PlayState.lua
+++ Flappy Adventure/states/PlayState.lua
@@ -1,15 +1,62 @@
-TitleScreenState = Class{__includes = BaseState}
+PlayState = Class{__includes = BaseState}
 
-function TitleScreenState:update(dt)
-    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        gStateMachine:change('play')
+PIPE_SPEED = 60
+PIPE_WIDTH = 70
+PIPE_HEIGHT = 288
+
+BIRD_WIDTH = 38
+BIRD_HEIGHT = 24
+
+function PlayState:init()
+    self.bird = Bird()
+    self.pipePairs = {}
+    self.timer = 0
+
+    self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+end
+
+function PlayState:update(dt)
+    self.timer = self.timer + dt
+
+    if self.timer > 2 then
+        local y = math.max(-PIPE_HEIGHT + 10, 
+            math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        self.lastY = y
+
+        table.insert(self.pipePairs, PipePair(y))
+
+        self.timer = 0
+    end
+
+    for k, pair in pairs(self.pipePairs) do
+        pair:update(dt)
+    end
+
+    for k, pair in pairs(self.pipePairs) do
+        if pair.remove then
+            table.remove(self.pipePairs, k)
+        end
+    end
+
+    self.bird:update(dt)
+
+    for k, pair in pairs(self.pipePairs) do
+        for l, pipe in pairs(pair.pipes) do
+            if self.bird:collides(pipe) then
+                gStateMachine:change('title')
+            end
+        end
+    end
+
+    if self.bird.y > VIRTUAL_HEIGHT - 15 then
+        gStateMachine:change('title')
     end
 end
 
-function TitleScreenState:render()
-    love.graphics.setFont(flappyFont)
-    love.graphics.printf('Flappy Adventure', 0, 64, VIRTUAL_WIDTH, 'center')
+function PlayState:render()
+    for k, pair in pairs(self.pipePairs) do
+        pair:render()
+    end
 
-    love.graphics.setFont(mediumFont)
-    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+    self.bird:render()
 end
