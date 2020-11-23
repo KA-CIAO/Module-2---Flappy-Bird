f819decf70b2ef43e822bb0b531ee592fe80cb20
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-11-23 17:59:54 +0800

    Update PlayState.lua

--- Flappy Adventure/states/PlayState.lua
+++ Flappy Adventure/states/PlayState.lua
@@ -34,6 +34,7 @@ function PlayState:update(dt)
             if pair.x + PIPE_WIDTH < self.bird.x then
                 self.score = self.score + 1
                 pair.scored = true
+                sounds['score']:play()
             end
         end
         
@@ -46,19 +47,23 @@ function PlayState:update(dt)
         end
     end
 
-    self.bird:update(dt)
-
     for k, pair in pairs(self.pipePairs) do
         for l, pipe in pairs(pair.pipes) do
             if self.bird:collides(pipe) then
+                sounds['explosion']:play()
+                sounds['hurt']:play()
                gStateMachine:change('score', {
                     score = self.score
                 })
             end
         end
     end
+    
+    self.bird:update(dt)
 
     if self.bird.y > VIRTUAL_HEIGHT - 15 then
+        sounds['explosion']:play()
+        sounds['hurt']:play()
         gStateMachine:change('score', {
             score = self.score
         })
@@ -75,3 +80,12 @@ function PlayState:render()
     
     self.bird:render()
 end
+
+function PlayState:enter()
+    scrolling = true
+end
+
+function PlayState:exit()
+    scrolling = false
+end
+    
