5466cfce87ec62c6e7860d995ad8182889ac4c47
JoshuaLacambacal <72681106+JoshuaLacambacal@users.noreply.github.com>
2021-02-15 23:38:12 +0800

    Add files via upload

---
+++ Flappy Adventure(Updated)/states/PlayState.lua
@@ -0,0 +1,112 @@
+PlayState = Class{__includes = BaseState}
+
+PIPE_SPEED = 60
+PIPE_WIDTH = 70
+PIPE_HEIGHT = 288
+
+BIRD_WIDTH = 38
+BIRD_HEIGHT = 24
+
+function PlayState:init()
+    self.bird = Bird()
+    self.pipePairs = {}
+    self.timer = 0
+    self.score = 0
+
+    self.lastY = -PIPE_HEIGHT + math.random(90) + 20
+end
+
+function PlayState:update(dt)
+
+        if scrolling == true and love.keyboard.wasPressed('p') then
+        sounds['pause']:play()
+        scrolling = false
+
+    elseif scrolling == false and love.keyboard.wasPressed('p') then
+        scrolling = true
+    end
+
+    self.timer = self.timer + dt
+
+    if scrolling then
+        while self.timer > math.random(100) do
+            local y = math.max(-PIPE_HEIGHT + 10, 
+                math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            self.lastY = y
+
+            table.insert(self.pipePairs, PipePair(y))
+
+            self.timer = 0
+        end
+    end
+
+    if scrolling then
+        for k, pair in pairs(self.pipePairs) do
+            if not pair.scored then
+                if pair.x + PIPE_WIDTH < self.bird.x then
+                    self.score = self.score + 1
+                    pair.scored = true
+                    sounds['score']:play()
+                end
+            end
+
+            pair:update(dt)
+        end
+    end
+
+
+    for k, pair in pairs(self.pipePairs) do
+        if pair.remove then
+            table.remove(self.pipePairs, k)
+        end
+    end
+
+    if scrolling then
+        for k, pair in pairs(self.pipePairs) do
+            for l, pipe in pairs(pair.pipes) do
+                if self.bird:collides(pipe) then
+                    sounds['explosion']:play()
+                    sounds['hurt']:play()
+
+                    gStateMachine:change('score', {
+                        score = self.score
+                    })
+                end
+            end
+        end
+
+        self.bird:update(dt)
+    end
+
+    if self.bird.y > VIRTUAL_HEIGHT - 15 then
+        sounds['explosion']:play()
+        sounds['hurt']:play()
+
+        gStateMachine:change('score', {
+            score = self.score
+        })
+    end
+end
+
+function PlayState:render()
+    for k, pair in pairs(self.pipePairs) do
+        pair:render()
+    end
+
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
+    self.bird:render()
+    if scrolling == false then
+        love.graphics.printf('PAUSED', 0, 115, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press P to resume', 0, 150, VIRTUAL_WIDTH, 'center')
+    end
+end
+
+function PlayState:enter()
+    scrolling = true
+end
+
+function PlayState:exit()
+    scrolling = false
+end
---
+++ Flappy Adventure(Updated)/states/ScoreState.lua
@@ -0,0 +1,47 @@
+ScoreState = Class{__includes = BaseState}
+
+function ScoreState:enter(params)
+    self.score = params.score
+end
+
+function ScoreState:init()
+    self.bronze = love.graphics.newImage('Bronze.png')
+    self.width = self.bronze:getWidth()
+    self.height = self.bronze:getHeight()
+    self.silver = love.graphics.newImage('Silver.png')
+    self.width = self.silver:getWidth()
+    self.height = self.silver:getHeight()
+    self.gold = love.graphics.newImage('Gold.png')
+    self.width = self.gold:getWidth()
+    self.height = self.gold:getHeight()
+    self.x = VIRTUAL_WIDTH / 2 - 12
+end
+
+function ScoreState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function ScoreState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+
+   if self.score > 5 and self.score < 11 then
+        love.graphics.draw(self.bronze, self.x, 180)
+        love.graphics.printf ('You got a Bronze Medal, Keep Practicing! :)', 0, 130, VIRTUAL_WIDTH, 'center')
+
+    elseif self.score > 10 and self.score < 21 then
+        love.graphics.draw(self.silver, self.x, 180)
+        love.graphics.printf ('You got a Silver Medal, Keep Up the Good Work! :)', 0, 130, VIRTUAL_WIDTH, 'center')
+
+    elseif self.score > 20 then
+        love.graphics.draw(self.gold, self.x, 180)
+        love.graphics.printf ('Congratulations! You got a Gold Medal! :)', 0, 130, VIRTUAL_WIDTH, 'center')
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+end
